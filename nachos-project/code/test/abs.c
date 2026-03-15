a8a8ee9aae42296ea8225402e2ef06a6f925e32a
Mr-Suave <anvayjoshi5@gmail.com>
2026-03-15 16:54:41 +0530

    Absolute function implemented & tested

--- nachos-project/code/test/abs.c
+++ nachos-project/code/test/abs.c
@@ -4,7 +4,9 @@ int main() {
     int result;
 
     result = Abs(-65);
-
+    PrintString("Printing the absolute value of -65: ");
+    PrintNum(result);
+    PrintString("\n");
     Halt();
     /* not reached */
 }
